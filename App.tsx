37ec9d4f3c29ac96ba25781fc6654eefcfe64138
mauripavan <mauriciopavan60@gmail.com>
2023-12-27 12:02:29 -0300

    feat: react navigation config

--- App.tsx
+++ App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import styled, {ThemeProvider} from 'styled-components';
+import {ThemeProvider} from 'styled-components';
+import {NavigationContainer} from '@react-navigation/native';
 
 import theme from './src/theme';
+import RootNavigation from './src/navigation';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <StyledView>
-        <Text>Styled Components initial config</Text>
-      </StyledView>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-const StyledView = styled(View)`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({theme}) => theme.colors.green[0]};
-`;
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {StyledView} from './styles';
+
+const HomeScreen = () => {
+  return (
+    <StyledView>
+      <Text>React Navigation initial config</Text>
+    </StyledView>
+  );
+};
+
+export default HomeScreen;
