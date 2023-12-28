1ce1a459bc28e4e9ee9aee93791d1381b436b671
mauripavan <mauriciopavan60@gmail.com>
2023-12-28 16:42:38 -0300

    refactor: updates on home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,23 +1,35 @@
 import React, {useEffect, useState} from 'react';
 import {useTheme} from 'styled-components';
 import {FlatList, SafeAreaView} from 'react-native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useNavigation} from '@react-navigation/native';
 
 import {getArtWorks} from '../../utils/api';
 import Separator from '../../components/Separator';
 import {pixelSizeHorizontal, pixelSizeVertical} from '../../utils/metrics';
 import MainCard from '../../components/MainCard';
 import {IArtWorkProps} from './types';
+import Loading from '../../components/Loading';
+import {LoadingSize} from '../../components/Loading/types';
+
+export type HomeScreenParamList = {
+  HomeScreen: {itemId: number} | undefined;
+};
 
 const HomeScreen = () => {
   const {colors} = useTheme();
   const [artWorks, setArtWorks] = useState<Array<IArtWorkProps>>([]);
   const [baseIIIF, setBaseIIF] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
 
   useEffect(() => {
+    setLoading(true);
     getArtWorks().then(response => {
       setArtWorks(response.data);
       setBaseIIF(response.config.iiif_url);
     });
+    setLoading(false);
   }, []);
 
   const renderArtItems = ({
@@ -28,8 +40,22 @@ const HomeScreen = () => {
     index: number;
   }) => {
     const imageUrl = `${baseIIIF}/${item.image_id}/full/400,/0/default.jpg`;
-    return <MainCard item={item} index={index} imageUrl={imageUrl} />;
+    const handleItemPress = (itemId: number) => {
+      navigation.navigate('SingleItemScreen', {itemId});
+    };
+    return (
+      <MainCard
+        item={item}
+        index={index}
+        imageUrl={imageUrl}
+        onPress={() => handleItemPress(item.id)}
+      />
+    );
   };
+
+  if (loading) {
+    return <Loading size={LoadingSize.LARGE} color={colors.green[0]} />;
+  }
 
   return (
     <SafeAreaView style={{backgroundColor: colors.white}}>
