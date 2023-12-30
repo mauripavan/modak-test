d0ebd6bd486b92b677934c9f486b0be8f0aabf61
mauripavan <mauriciopavan60@gmail.com>
2023-12-30 18:06:35 -0300

    feat: packages installation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {NavigationContainer} from '@react-navigation/native';
 import {RecoilRoot} from 'recoil';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import theme from './src/theme';
 import RootNavigation from './src/navigation';
@@ -9,11 +10,13 @@ import RootNavigation from './src/navigation';
 function App(): React.JSX.Element {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <RootNavigation />
-        </NavigationContainer>
-      </ThemeProvider>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <ThemeProvider theme={theme}>
+          <NavigationContainer>
+            <RootNavigation />
+          </NavigationContainer>
+        </ThemeProvider>
+      </GestureHandlerRootView>
     </RecoilRoot>
   );
 }
